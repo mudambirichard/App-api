f6a546a5a07d7c19f8cdb0e074460d5020eb5c4c
mudambi richard <mudarichards@gmail.com>
2018-05-29 14:58:25 +0300

    modify app.py

--- app.py
+++ app.py
@@ -16,12 +16,6 @@ requests = [
 def get_requests( ):
     return jsonify({'requests': requests})
 
-@app.route('/api/v1.0/requests/<int:requests_id>', methods=['GET'])
-def get_requests(request_id):
-    requests = [requests for requests in requests if requests['id'] == task_id]
-    if len(requests) == 0:
-        abort(404)
-    return jsonify({'requests' : requests[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
