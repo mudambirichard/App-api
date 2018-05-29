4b861edd62ecacff340e6b2cb63e255878f4d5fa
mudambi richard <mudarichards@gmail.com>
2018-05-29 15:55:09 +0300

    modify test.py

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from app import app
 import unittest
+class FlaskTestCase(unittest.TestCase):
 
-def test_requests(self):
-    tester = app.test_client(self)
-    response = tester.requests('/api/v1.0/requests')
-    self.assertFalse( response.data )
+    def test_requests(self):
+        tester = app.test_client(self)
+        response = tester.requests('/api/v1.0/requests')
+        self.assertFalse( response.data )
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
