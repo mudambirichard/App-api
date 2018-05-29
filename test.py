d109ffb7cf16ba4b4f081344e319dc4e68a756ac
mudambi richard <mudarichards@gmail.com>
2018-05-29 19:51:42 +0300

    modify app.py

--- test.py
+++ test.py
@@ -8,7 +8,10 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.requests('/api/v1.0/users/requests', methods=['GET'])
         self.assertFalse( response.data )
 
-
+    def test_requests(self):
+        tester = app.test_client(self)
+        response = tester.requests('/api/v1.0/users/requests', methods=['POST'])
+        self.assertFalse( response.data )
 
 
 if __name__ == '__main__':
