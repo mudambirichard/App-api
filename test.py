477ee834cd5b6d50de8ea502a2b61ae7c6e29896
mudambi richard <mudarichards@gmail.com>
2018-05-29 17:56:57 +0300

    modify test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import os
 from app import app
 import unittest
 class FlaskTestCase(unittest.TestCase):
 
     def test_requests(self):
         tester = app.test_client(self)
-        response = tester.requests('/api/v1.0/requests')
+        response = tester.requsets('/api/v1.0/users/requsets', methods=['GET'])
         self.assertFalse( response.data )
 
 
