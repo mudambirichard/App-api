188c3ee6661c29fc23270712fc756b749169be13
mudambirichard <muidarichards@gmail.com>
2018-05-30 13:23:47 +0300

    modify test.py

--- test.py
+++ test.py
@@ -1,17 +1,38 @@
-import os
 from app import app
 import unittest
+import requests
+import json
+
+BASE_URL = 'http://127.0.0.15000/api/v1/users/requests'
+BAD_ITEM_URL = '{}/5'.format(BASE_URL)
+GOOD_ITEM_URL ='{}/3'.format(BASE_URL)
 class FlaskTestCase(unittest.TestCase):
 
-    def test_requests(self):
-        tester = app.test_client(self)
-        response = tester.requests('/api/v1.0/users/requests', methods=['GET'])
-        self.assertFalse( response.data )
 
-    def test_requests(self):
+    def test_user_request(self):
         tester = app.test_client(self)
-        response = tester.requests('/api/v1.0/users/requests', methods=['POST'])
-        self.assertFalse( response.data )
+        response = tester.get('http://127.0.0.15000/api/v1.0/users/requests')
+        self.assertEqual( response.status_code, 200 )
+
+    def test_user_request(self):
+        tester = app.test_client(self)
+        response = tester.get('http://127.0.0.15000/api/v1.0/users/requests/<int:user_id>')
+        self.assertEqual(response.status_code, 404 )
+
+    def test_create_request(self):
+        tester = app.test_client(self)
+        response = tester.post('/api/v1/users/requests',data=json.dumps(
+            dict(name="muda", username="richard", email="me@gmail.com")),
+            content_type="application/json")
+        self.assertEqual(response.status_code, 404)
+    
+
+    def test_update_user(self):
+        tester = app.test_client(self)
+        response = tester.put('/api/v1/users/requests/<int:user_id>',data=json.dumps(
+            dict(name="kaka", username="boyco", email="me@gmail.com")),
+            content_type="application/json")
+        self.assertEqual(response.status_code, 405)
 
 
 if __name__ == '__main__':
