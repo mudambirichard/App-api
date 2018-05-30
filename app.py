f35a78b66c2c338912cdd7950c6935e7eb7b3404
mudambirichard <muidarichards@gmail.com>
2018-05-30 14:39:03 +0300

    modify app.py

--- app.py
+++ app.py
@@ -60,6 +60,25 @@ def create_request():
     user.append(usr)
     return jsonify({'user' : users})
 
+#this method modify the brequest.
+@app.route('/api/v1/users/requests/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = [user for user in users if users['id'] == user_id]
+    if len(user) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'name' in request.json and type (request.json['name']) != unicode:
+        abort(400)
+    if 'password' in request.json and type (request.json['password']) != unicode:
+        abort(400)
+    if 'email' in request.json and type (request.json['email']) != unicode:
+        abort(400)
+    user[0]['user'] = request.json.get('name',user[0]['name'])
+    user[0]['user'] = request.json.get('password',user[0]['password'])
+    user[0]['user'] = request.json.get('email',user[0]['email'])
+    return  jsonify({'user':user[0]})
+
 
 
 
