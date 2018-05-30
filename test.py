dcee0b308fbdcfac0aa8714bc9556b97ac8b9698
mudambirichard <muidarichards@gmail.com>
2018-05-30 12:48:59 +0300

    modifyapp.py

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
-import os
 from app import app
 import unittest
+import requests
+import json
+
+BASE_URL = 'http://127.0.0.15000/api/v1/users/requests'
+BAD_ITEM_URL = '{}/5'.format(BASE_URL)
+GOOD_ITEM_URL ='{}/3'.format(BASE_URL)
 class FlaskTestCase(unittest.TestCase):
 
-    def test_requests(self):
+
+    def test_user_request(self):
         tester = app.test_client(self)
-        response = tester.requsets('/api/v1.0/users/requsets', methods=['GET'])
-        self.assertFalse( response.data )
+        response = tester.get('http://127.0.0.15000/api/v1.0/users/requests')
+        self.assertEqual( response.status_code, 200 )
 
-
+    def test_user_request(self):
+        tester = app.test_client(self)
+        response = tester.get('http://127.0.0.15000/api/v1.0/users/requests/<int:user_id>')
+        self.assertEqual(response.status_code, 404 )
 
 
 if __name__ == '__main__':
